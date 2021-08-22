84a673ffdf2306ff4f83295c3db6cfd404bde060
bowdenshot <anthonyelavezzo@gmail.com>
2021-08-22 14:37:31 -0700

    Manager constructor and tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,3 +1,4 @@
+const { test } = require('@jest/globals');
 const Manager = require('../lib/Manager')
 
 
@@ -7,4 +8,10 @@ test('creates an Manager Object', () => {
     expect(manager.name).toBe('Mark');
     expect(manager.id).toBe(129312);
     expect(manager.email).toBe('mark@gmail.com')
+});
+
+test('getRole returns Manager', () => {
+    const manager = new Manager('Mark', 129312, 'mark@gmail.com');
+
+    expect(manager.getRole()).toBe('Manager');
 });
